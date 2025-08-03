2f95bc598a5ac83e82bbe744f8556cdf6cef3505
Selvakumar Gnanavel <selva.735@gmail.com>
2025-08-03 23:46:58 +0530

    Create data_preprocessing.py

---
+++ src/data_preprocessing.py
@@ -0,0 +1,14 @@
+# Data Preprocessing Module
+# This module contains functions for preprocessing DTI data
+
+def preprocess_data():
+    """
+    Function to preprocess drug-target interaction data
+    """
+    pass
+
+def clean_data():
+    """
+    Function to clean and validate data
+    """
+    pass
